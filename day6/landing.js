ac8de5ce687decbef87ec635f78e4fb0a19543d2
divyaodeyar <divyaodiyar115@gmail.com>
2025-08-22 19:21:52 +0530

    commit

--- day6/landing.js
+++ day6/landing.js
@@ -1,22 +1,30 @@
 const con =document.getElementById("container")
-const arr =[".assets/coffee2.jpg",
+const arr =["./assets/coffee2.jpg",
 "./assets/coffee3.webp",
-"./assets/coffee4.jpg",
+"./assets/coffee44.jpg",
 "./assets/coffee5.jpg",
 "./assets/coffee6.jpg"
 ]
 
+const img = document.getElementsByClassName('img');
+console.log(img);
+
 let i=0;
 let next=()=>{
+    img[i].classList.remove('active');
     i++;
+
     if(i==arr.length)
     {
         i=0;
     }
+    img[i].classList.add('active');
     
 
     con.style.backgroundImage=`url('${arr[i]}')`;
 }
+
+
 let prev=()=>{
     i--;//i=-1
     if(i==-1)
